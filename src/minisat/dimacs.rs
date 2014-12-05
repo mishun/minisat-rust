6402bc6637f0a380b6d4cd7827ebdf1d63086ce3
Alexander Mishunin <alexander.mishunin@gmail.com>
2014-12-06 03:05:13 +0400

    Better io error handling when parsing

--- src/minisat/dimacs.rs
+++ src/minisat/dimacs.rs
@@ -1,73 +1,69 @@
 use std::num::{SignedInt};
-use std::io::{Buffer};
+use std::io;
 use super::literal::{Var, Lit};
 use super::lbool::{LBool};
 use super::core_solver::{Solver};
 use super::index_map::{HasIndex};
 
 
-fn parse_clause<B : Buffer>(p : &mut BufferParser<B>) -> Option<Vec<Lit>> {
+fn parse_clause<B : Buffer>(p : &mut BufferParser<B>) -> io::IoResult<Vec<Lit>> {
     let mut lits : Vec<Lit> = Vec::new();
     loop {
-        let tmp = p.nextInt();
-        match tmp {
-            None      => { return None; }
-            Some(0)   => { return Some(lits); }
-            Some(lit) => {
-                let var = lit.abs() as uint - 1;
-                lits.push(Lit::new(Var::new(var), lit < 0));
-            }
+        let lit = try!(p.nextInt());
+        if lit == 0 {
+            return Ok(lits);
+        } else {
+            let var = lit.abs() as uint - 1;
+            lits.push(Lit::new(Var::new(var), lit < 0));
         }
     }
 }
 
-pub fn parse_DIMACS<B : Buffer, S : Solver>(buffer : &mut B, solver : &mut S, validate : bool) -> bool {
-    let mut p = BufferParser::new(buffer);
+pub fn parse_DIMACS<B : io::Buffer, S : Solver>(buffer : &mut B, solver : &mut S, validate : bool) -> io::IoResult<()> {
+    let mut p = try!(BufferParser::new(buffer));
 
     enum State { Waiting, Parsing(uint, uint) }
 
     let mut state = State::Waiting;
     loop {
-        p.skipWhitespace();
+        try!(p.skipWhitespace());
         match state {
             State::Waiting => {
                 match p.current() {
-                    Some('c')      => { p.skipLine(); }
-                    Some('p')      => {
-                        if !p.consume("p cnf") { return false; }
-                        let vars = p.nextUInt();
-                        let clauses = p.nextUInt();
-                        if vars.is_none() || clauses.is_none() { return false; }
-                        state = State::Parsing(vars.unwrap(), clauses.unwrap());
+                    Some('c') => { try!(p.skipLine()); }
+                    _         => {
+                        try!(p.consume("p cnf"));
+                        let vars = try!(p.nextUInt());
+                        let clauses = try!(p.nextUInt());
+                        state = State::Parsing(vars, clauses);
                     }
-                    None | Some(_) => { return false; }
                 }
             }
 
             State::Parsing(vars, clauses) => {
                 match p.current() {
-                    Some('c') => { p.skipLine(); }
+                    Some('c') => { try!(p.skipLine()); }
 
                     None      => {
                         if validate && (clauses != solver.nClauses() || vars < solver.nVars()) {
-                            println!("PARSE ERROR! DIMACS header mismatch");
-                            return false;
+                            return Err(
+                                io::IoError {
+                                    kind   : io::OtherIoError,
+                                    desc   : "PARSE ERROR! DIMACS header mismatch",
+                                    detail : None
+                                });
                         }
-                        return true;
+                        return Ok(());
                     }
 
                     _         => {
-                        match parse_clause(&mut p) {
-                            None    => { return false; }
-                            Some(c) => {
-                                for l in c.iter() {
-                                    while l.var().toIndex() >= solver.nVars() {
-                                        solver.newVar(LBool::Undef(), true);
-                                    }
-                                }
-                                solver.addClause(c.as_slice());
+                        let c = try!(parse_clause(&mut p));
+                        for l in c.iter() {
+                            while l.var().toIndex() >= solver.nVars() {
+                                solver.newVar(LBool::Undef(), true);
                             }
                         }
+                        solver.addClause(c.as_slice());
                     }
                 }
             }
@@ -83,52 +79,66 @@ pub struct BufferParser<'r, R : 'r> {
 }
 
 impl<'r, R : Buffer> BufferParser<'r, R> {
-    pub fn new(r : &'r mut R) -> BufferParser<'r, R> {
+    pub fn new(r : &'r mut R) -> io::IoResult<BufferParser<'r, R>> {
         let mut p : BufferParser<R> = BufferParser { reader : r, cur : None };
-        p.next();
-        p
+        try!(p.next());
+        Ok(p)
     }
 
-    pub fn next(&mut self) {
-        self.cur = self.reader.read_char().ok()
+    #[inline]
+    pub fn next(&mut self) -> io::IoResult<()> {
+        match self.reader.read_char() {
+            Ok(c)                                         => { self.cur = Some(c); Ok(()) }
+            Err(io::IoError { kind : io::EndOfFile, .. }) => { self.cur = None; Ok(()) }
+            Err(err)                                      => { Err(err) }
+        }
     }
 
+    #[inline]
     pub fn current(&self) -> Option<char> {
         self.cur
     }
 
-    pub fn skipWhitespace(&mut self) {
+    pub fn skipWhitespace(&mut self) -> io::IoResult<()> {
         loop {
             match self.cur {
                 None                          => break,
                 Some(c) if !c.is_whitespace() => break,
-                _                             => self.next()
+                _                             => try!(self.next())
             }
         }
+        Ok(())
     }
 
-    pub fn skipLine(&mut self) {
+    pub fn skipLine(&mut self) -> io::IoResult<()> {
         loop {
             match self.cur {
                 None       => break,
-                Some('\n') => { self.next(); break; }
-                _          => self.next()
+                Some('\n') => { try!(self.next()); break; }
+                _          => { try!(self.next()) }
             }
         }
+        Ok(())
     }
 
-    pub fn consume(&mut self, target : &str) -> bool {
+    pub fn consume(&mut self, target : &str) -> io::IoResult<()> {
         for tc in target.chars() {
             match self.cur {
-                Some(c) if c == tc => { self.next() }
-                _                  => { return false }
+                Some(c) if c == tc => { try!(self.next()) }
+                _                  => {
+                    return Err(
+                        io::IoError {
+                            kind   : io::OtherIoError,
+                            desc   : "failed to consume",
+                            detail : Some(format!("expected '{}'", target))
+                        });
+                }
             }
         }
-        true
+        Ok(())
     }
 
-
-    fn readIntBody(&mut self) -> Option<uint> {
+    fn readIntBody(&mut self) -> io::IoResult<uint> {
         let mut len : uint = 0;
         let mut value = 0;
         loop {
@@ -136,33 +146,42 @@ impl<'r, R : Buffer> BufferParser<'r, R> {
                 Some(d)      => {
                     value = value * 10 + d;
                     len += 1;
-                    self.next();
+                    try!(self.next())
                 }
-                _ if len > 0 => return Some(value),
-                _            => return None
+
+                _ if len > 0 => { return Ok(value) }
+
+                _            => {
+                    return Err(
+                        io::IoError {
+                            kind   : io::OtherIoError,
+                            desc   : "int expected",
+                            detail : None
+                        });
+                }
             }
         }
     }
 
-    pub fn nextInt(&mut self) -> Option<int> {
-        self.skipWhitespace();
+    pub fn nextInt(&mut self) -> io::IoResult<int> {
+        try!(self.skipWhitespace());
         let sign =
             match self.cur {
-                Some('+') => { self.next(); 1 }
-                Some('-') => { self.next(); -1 }
+                Some('+') => { try!(self.next()); 1 }
+                Some('-') => { try!(self.next()); -1 }
                 _         => 1
             };
 
-        self.readIntBody().map(|val| { sign * val as int })
+        let val = try!(self.readIntBody());
+        Ok(sign * val as int)
     }
 
-    pub fn nextUInt(&mut self) -> Option<uint> {
-        self.skipWhitespace();
+    pub fn nextUInt(&mut self) -> io::IoResult<uint> {
+        try!(self.skipWhitespace());
         match self.cur {
-            Some('+') => { self.next() }
+            Some('+') => { try!(self.next()) }
             _         => {}
         }
         self.readIntBody()
     }
 }
-
